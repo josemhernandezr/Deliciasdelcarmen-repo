c4795a77c5c23b5ba4891458618039487851fc1f
josemhernandezr <77945432+josemhernandezr@users.noreply.github.com>
2021-04-03 18:10:22 -0600

    last update

--- deliciasdelcarmen/src/App.js
+++ deliciasdelcarmen/src/App.js
@@ -1,8 +1,12 @@
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import NavBar from './Components/NavBar/NavBar';
 function App() {
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <NavBar></NavBar>
+      <h1>Delicias del Carmen</h1>
     </div>
   );
 }
