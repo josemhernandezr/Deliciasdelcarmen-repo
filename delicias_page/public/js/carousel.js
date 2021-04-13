889f630d88f184d85f1da5b49e89805677932957
josemhernandezr <77945432+josemhernandezr@users.noreply.github.com>
2021-04-12 19:16:57 -0600

    update

---
+++ delicias_page/public/js/carousel.js
@@ -0,0 +1,17 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
